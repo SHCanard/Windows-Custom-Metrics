fc00e777c2b6be718c56f4917411b39b41f2876d
SHCanard <36749089+SHCanard@users.noreply.github.com>
2022-10-24 13:45:07 +0200

    Update get_sql_version.ps1

--- get_sql_version.ps1
+++ get_sql_version.ps1
@@ -2,11 +2,21 @@
 Write-Verbose -Message "Get SQL version..."
 $FullVersion=Invoke-Sqlcmd -Query "SELECT @@VERSION;" -QueryTimeout 3
 
+#If the previous command fails (no default instance), then look for the first instance
+if (!$FullVersion)
+{
+    $FirstInstance=(get-itemproperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances[0]
+    if ($FirstInstance)
+    {
+    	$FullVersion=Invoke-Sqlcmd -ServerInstance "localhost\$FirstInstance" -Query "SELECT @@VERSION;" -QueryTimeout 3
+    }
+}
+
 # If Version is found
 if ($FullVersion)
 {
 # Do some cleanup of the output
-	$SplitedOnSpace=($FullVersion | Select-Object -ExpandProperty Column1 | Out-String).Trim().Split(" ")
+  $SplitedOnSpace=($FullVersion | Select-Object -ExpandProperty Column1 | Out-String).Trim().Split(" ")
   $SplitedOnParenthesis=$SplitedOnSpace[4].Split("()")  
     
 # Display if not empty
